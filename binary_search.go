7a4d7a8316bac6226fb20ce227f5445bf6173f39
Kyumamoon <kyumamoonsocial@gmail.com>
2023-01-07 19:19:44 -0800

    Added recursiveBinarySearch function.

--- binary_search.go
+++ binary_search.go
@@ -6,6 +6,7 @@ import (
 	"sort"
 )
 
+// binarySearch returns the index and true or false whether the target is found in the list.
 func binarySearch(list []int, target int) (int, bool) {
 	first := 0
 	last := len(list) - 1
@@ -27,14 +28,36 @@ func binarySearch(list []int, target int) (int, bool) {
 
 }
 
+// recursiveBinarySearch returns true or false whether target is found in the list using recursive.
+func recursiveBinarySearch(list []int, target int) bool {
+	if len(list) == 0 {
+		return false
+	} else {
+		midpoint := math.Floor(float64(len(list)) / 2)
+
+		if list[int(midpoint)] == target {
+			return true
+		} else if list[int(midpoint)] < target {
+			recursiveBinarySearch(list[int(midpoint)+1:], target)
+		} else {
+			recursiveBinarySearch(list[:int(midpoint)], target)
+		}
+	}
+
+	return false
+}
+
 func main() {
-	list := []int{1, 7, 2, 4, 3, 8, 6, 5, 9, 10}
-	// Sorted: 1,2,3,4,5,6,7,8,9,10
+	list := []int{1, 7, 2, 4, 3, 8, 6, 5}
+	// Sorted: 1,2,3,4,5,6,7,8
 	sort.Ints(list)
-	x, found := binarySearch(list, 10)
+	/*x, found := binarySearch(list, 10)
 	if found {
 		fmt.Println("INDEX:", x)
 	} else {
 		fmt.Println("NO VALUES FOUND.")
-	}
+	}*/
+
+	y := recursiveBinarySearch(list, 5)
+	fmt.Println("Target Found:", y)
 }
